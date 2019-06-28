ee0a580fd1188390ca27c40f0056b9daed478203
Yuriy Syrovetskiy <cblp@cblp.su>
2019-06-28 18:36:47 +0300

    Decouple object id and frame

--- ron/lib/RON/Util.hs
+++ ron/lib/RON/Util.hs
@@ -4,10 +4,19 @@
 module RON.Util (
     ByteStringL,
     Instance (Instance),
+    runStateAsWriter,
 ) where
+
+import           RON.Prelude
 
 import qualified Data.ByteString.Lazy as BSL
 
 type ByteStringL = BSL.ByteString
 
 data Instance c = forall a . c a => Instance a
+
+runStateAsWriter :: Semigroup s => MonadState s m => WriterT s m a -> m a
+runStateAsWriter action = do
+    (a, s) <- runWriterT action
+    modify' (s <>)
+    pure a
